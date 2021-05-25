3e5c140e05c14bea257de9a102cf71fdcf4aba38
angelomelgar <amelgargozzer@gmail.com>
2021-05-25 06:53:07 -0500

    Add files via upload

---
+++ mov-claims-ms-client/src/main/java/pe/com/movistar/movclaimsmsclient/MovClaimsMsClientApplication.java
@@ -0,0 +1,13 @@
+package pe.com.movistar.movclaimsmsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovClaimsMsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovClaimsMsClientApplication.class, args);
+	}
+
+}
---
+++ mov-claims-ms-client/src/test/java/pe/com/movistar/movclaimsmsclient/MovClaimsMsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.movistar.movclaimsmsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovClaimsMsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
